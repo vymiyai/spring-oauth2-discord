7f69279d111741bdb691ceb7397b5185a8643c4f
vymiyai <vymiyai@gmail.com>
2017-06-18 13:07:14 +0200

    Testing the FB authentication.

--- src/main/java/com/rhcloud/tutorials/spring_oauth2_discord/App.java
+++ src/main/java/com/rhcloud/tutorials/spring_oauth2_discord/App.java
@@ -1,49 +1,39 @@
 package com.rhcloud.tutorials.spring_oauth2_discord;
 
+import java.security.Principal;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class App {
-	/**
-	 * RESTful request handler for HTTP.
-	 * 
-	 * @return the list of command prefixes.
-	 */
-	@RequestMapping("/")
-	public String home() {
-		return "wololo";
+@EnableOAuth2Sso
+public class App extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
 	}
 
-	/**
-	 * Main method run statically.
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// get token as environment variable.
-		// final String token = System.getenv("gow_token");
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+				.authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		// @formatter:on
+	}
 
+	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-
-	/**
-	 * Bean that lists java Beans registered by Spring.
-	 * 
-	 * @param ctx
-	 * @return
-	 */
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-			System.out.println();
-		};
-	}
-
 }
