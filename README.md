ebdac7cce29b9ae7d263f8d7db15d1ebd25192b1
vymiyai <vymiyai@gmail.com>
2017-07-02 20:01:23 +0200

    Discord and Facebook.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # spring-oauth2-discord
-Creating a small Spring app to test oauth2 witht he Discord API.
+Creating a small Spring app to test oauth2 with he Discord API.
 
 https://spring.io/guides/tutorials/spring-boot-oauth2/
+
+Discord OAuth2 information:
+
+https://discordapp.com/developers/docs/topics/oauth2
+
+Discord apps:
+
+https://discordapp.com/developers/applications/me
+
+About creating certificates:
+
+http://nemerosa.ghost.io/2015/07/06/enabling-ssl-with-spring-boot/
--- src/main/java/com/rhcloud/tutorials/spring_oauth2_discord/SocialApplication.java
+++ src/main/java/com/rhcloud/tutorials/spring_oauth2_discord/SocialApplication.java
@@ -2,12 +2,9 @@ package com.rhcloud.tutorials.spring_oauth2_discord;
 
 import java.security.Principal;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -15,9 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableOAuth2Sso
-public class App extends WebSecurityConfigurerAdapter {
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
 
 	@RequestMapping("/user")
 	public Principal user(Principal principal) {
@@ -34,6 +31,7 @@ public class App extends WebSecurityConfigurerAdapter {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(SocialApplication.class, args);
 	}
+
 }
